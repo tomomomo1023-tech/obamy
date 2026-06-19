e0e1e4059d757dd7576a09c8b4223dd2000da9bf
tomomomo1023-tech <tomomomo1023@icloud.com>
2026-06-19 22:52:40 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Hiragino Sans",
+    "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+}
